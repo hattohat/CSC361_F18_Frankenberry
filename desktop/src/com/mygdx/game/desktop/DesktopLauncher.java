b9531d1d6e3ed29f7513b1e3d645dfb1e0217b51
Unknown <hattohat@gmail.com>
2018-10-31 12:23:08 -0400

    Added Initial Framework

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,11 +2,29 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
 import com.mygdx.game.CSC361_F18_Frankenberry;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new CSC361_F18_Frankenberry(), config);
+	private static boolean rebuildAtlas = false;
+	private static boolean drawDebugOutline = false;
+
+	public static void main(String[] args) {
+		if (rebuildAtlas) {
+			Settings settings = new Settings();
+			settings.maxWidth = 1024;
+			settings.maxHeight = 1024;
+			settings.duplicatePadding = false;
+			settings.debug = drawDebugOutline;
+			TexturePacker.process(settings, "assets-raw/images", "../CanyonBunny-android/assets/images", "canyonbunny.pack");
+		}
+
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "CanyonBunny";
+		cfg.width = 800;
+		cfg.height = 480;
+
+		new LwjglApplication(new CSC361_F18_Frankenberry(), cfg);
 	}
 }
